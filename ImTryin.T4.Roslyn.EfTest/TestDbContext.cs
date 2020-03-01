9c3d754294dae54e4ff266e8528d907579a9f631
Lex <lex.lar@gmail.com>
2020-03-02 00:09:09 +0300

    Initial version.

---
+++ ImTryin.T4.Roslyn.EfTest/TestDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImTryin.T4.Roslyn.EfTest
+{
+    public partial class TestDbContext : DbContext
+    {
+    }
+
+    public abstract partial class BaseEntity
+    {
+        public long Id { get; set; }
+    }
+
+    public partial class Author : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+
+    public partial class Book : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public virtual Author Author { get; set; }
+    }
+}
